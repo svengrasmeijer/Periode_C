40d8a4ede5bc641010ffbd0ea4b3bae57beed38a
svengrasmeijer <sven.grasmeijer@student.hu.nl>
2022-03-10 10:50:49 +0100

    Practicum 5

---
+++ src/Practicum_5/Klas.java
@@ -0,0 +1,25 @@
+package Practicum_5;
+import java.util.ArrayList;
+
+public class Klas {
+    private String klasCode;
+    private ArrayList<String> Leerling;
+
+    Klas(String kC) {
+        klasCode = kC;
+    }
+
+    public void voegLeerlingToe(Leerling l) {
+    }
+
+    public void wijzigCijfer(String nm, double nweCijfer) {
+    }
+
+    public int aantalLeerlingen() {
+        return 0;
+    }
+
+    public String toString() {
+        return null;
+    }
+}
---
+++ src/Practicum_5/Leerling.java
@@ -0,0 +1,22 @@
+package Practicum_5;
+
+public class Leerling {
+    private String naam;
+    private double cijfer;
+
+    Leerling(String nm) {
+        naam = nm;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public double getCijfer() {
+        return cijfer;
+    }
+
+    public void setCijfer(double c) {
+        cijfer = c;
+    }
+}
